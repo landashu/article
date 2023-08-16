42b9877d31e9e035aae323ea9950198d62d971f5
蓝大叔 <875730567@qq.com>
2023-08-16 15:10:37 +0800

    更新

--- docs/.vuepress/config/nav.ts
+++ docs/.vuepress/config/nav.ts
@@ -39,6 +39,7 @@ export default <Array<NavItem>>[
     items: [
       // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
       {text: 'MySQL', link: '/mysql/1300/',},
+      {text: 'MongoDB', link: '/mongodb/1800/',},
     ],
   },
   {
@@ -55,7 +56,8 @@ export default <Array<NavItem>>[
     text: '前端',
     items: [
       // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
-      {text: 'VUE3', link: '/vue3/1800/',},
+      {text: 'VUE3', link: '/vue3/1900/',},
+      {text: '微信小程序', link: '/wx/2000/',},
     ],
   },
 ];
